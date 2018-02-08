ebd1e9d08f3e433ee744201e8b30ded6ade20c14
Siddhartha Basu <biosidd@gmail.com>
2018-02-08 18:11:12 +0000

    Added struct for decoding orcid json response

--- user/user.go
+++ user/user.go
@@ -7,6 +7,7 @@ var (
 	Google   = "https://www.googleapis.com/userinfo/v2/me"
 	Facebook = "https://graph.facebook.com/v2.5/me?fields=name,email"
 	LinkedIn = "https://api.linkedin.com/v1/people/~:(first-name,last-name,email-address)?format=json"
+	Orcid    = "https://pub.orcid.org/v2.1"
 )
 
 type GoogleUser struct {
@@ -35,6 +36,60 @@ type LinkedInUser struct {
 		URL string `json:"url"`
 	} `json:"siteStandardProfileRequest"`
 	EmailAddress string `json:"emailAddress"`
+}
+
+type OrcidUser struct {
+	LastModifiedDate interface{} `json:"last-modified-date"`
+	Name             struct {
+		CreatedDate struct {
+			Value int64 `json:"value"`
+		} `json:"created-date"`
+		LastModifiedDate struct {
+			Value int64 `json:"value"`
+		} `json:"last-modified-date"`
+		GivenNames struct {
+			Value string `json:"value"`
+		} `json:"given-names"`
+		FamilyName struct {
+			Value string `json:"value"`
+		} `json:"family-name"`
+		CreditName interface{} `json:"credit-name"`
+		Source     interface{} `json:"source"`
+		Visibility string      `json:"visibility"`
+		Path       string      `json:"path"`
+	} `json:"name"`
+	OtherNames struct {
+		LastModifiedDate interface{}   `json:"last-modified-date"`
+		OtherName        []interface{} `json:"other-name"`
+		Path             string        `json:"path"`
+	} `json:"other-names"`
+	Biography      interface{} `json:"biography"`
+	ResearcherUrls struct {
+		LastModifiedDate interface{}   `json:"last-modified-date"`
+		ResearcherURL    []interface{} `json:"researcher-url"`
+		Path             string        `json:"path"`
+	} `json:"researcher-urls"`
+	Emails struct {
+		LastModifiedDate interface{}   `json:"last-modified-date"`
+		Email            []interface{} `json:"email"`
+		Path             string        `json:"path"`
+	} `json:"emails"`
+	Addresses struct {
+		LastModifiedDate interface{}   `json:"last-modified-date"`
+		Address          []interface{} `json:"address"`
+		Path             string        `json:"path"`
+	} `json:"addresses"`
+	Keywords struct {
+		LastModifiedDate interface{}   `json:"last-modified-date"`
+		Keyword          []interface{} `json:"keyword"`
+		Path             string        `json:"path"`
+	} `json:"keywords"`
+	ExternalIdentifiers struct {
+		LastModifiedDate   interface{}   `json:"last-modified-date"`
+		ExternalIdentifier []interface{} `json:"external-identifier"`
+		Path               string        `json:"path"`
+	} `json:"external-identifiers"`
+	Path string `json:"path"`
 }
 
 type NormalizedUser struct {
