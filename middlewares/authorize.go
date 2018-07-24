73beda81d9de5e6c5a27f63a46cb989440e7e2a6
Siddhartha Basu <biosidd@gmail.com>
2018-07-24 01:06:25 +0000

    Added check for X-Auth-Request-Redirect header in case of missing Original-Uri method

--- middlewares/authorize.go
+++ middlewares/authorize.go
@@ -2,28 +2,12 @@ package middlewares
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 	"strings"
 )
 
 func AuthorizeMiddleware(h http.Handler) http.Handler {
 	fn := func(w http.ResponseWriter, r *http.Request) {
-		var hStr []string
-		url := fmt.Sprintf("%v %v %v", r.Method, r.URL, r.Proto)
-		hStr = append(hStr, url)
-
-		// Add the host
-		hStr = append(hStr, fmt.Sprintf("Host: %v", r.Host))
-		// Loop through headers
-		for name, headers := range r.Header {
-			for _, h := range headers {
-				hStr = append(hStr, fmt.Sprintf("%v: %v", name, h))
-			}
-		}
-		log.Printf("\n ***** Headers **** \n%s\n", strings.Join(hStr, "\n"))
-		log.Println("\n **** End **** ")
-
 		hdr := r.Header
 		if hdr.Get("X-Scheme") != "https" {
 			http.Error(
@@ -41,12 +25,14 @@ func AuthorizeMiddleware(h http.Handler) http.Handler {
 			w.Write([]byte("passthrough for GET method"))
 			return
 		}
-		log.Printf("original uri %s\n", hdr.Get("X-Original-Uri"))
 		if strings.HasPrefix(hdr.Get("X-Original-Uri"), "/tokens") {
 			w.Write([]byte("no validation for /tokens"))
 			return
 		}
-		log.Println("going for JWT check")
+		if strings.HasPrefix(hdr.Get("X-Auth-Request-Redirect"), "/tokens") {
+			w.Write([]byte("no validation for /tokens"))
+			return
+		}
 		h.ServeHTTP(w, r)
 	}
 	return http.HandlerFunc(fn)
